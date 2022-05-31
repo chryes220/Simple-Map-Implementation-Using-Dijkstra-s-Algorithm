5719a3e1a7ead79b506e018f95f9caec8b45e8a9
= <christinehutabarat12@gmail.com>
2022-05-31 23:54:38 +0700

    fix dijkstra algorithm

--- src/backend/dijkstra.py
+++ src/backend/dijkstra.py
@@ -1,8 +1,10 @@
 # Dijkstra algorithm
 import networkx as nx
 import graph
+import time
 
 def dijkstra(start, end, graph):
+    begin = time.time()
     nodes = list(graph.nodes)
     visited = []
     expand = []
@@ -18,13 +20,19 @@ def dijkstra(start, end, graph):
     for n in list(graph[start]):
         dist[n] = graph[start][n]["weight"]
         prev[n] = start
-        expand = sorted(dist, key=dist.get)
-        for e in expand:
-            if (e in visited or e not in list(graph[start])):
-                expand.remove(e)
+    expand = sorted(dist, key=dist.get)
+    
+    i = 0
+    while i < len(expand):
+        if (expand[i] in visited or expand[i] not in list(graph[start])):
+            expand.pop(i)
+        else :
+            i += 1
     visited += start
+    it_count += 1
 
-    while end not in visited:
+    while end not in visited and len(expand) > 0:
+        print(expand)
         c_node = expand.pop(0)
         for n in list(graph[c_node]):
             new_dist = dist[c_node] + graph[c_node][n]["weight"]
@@ -33,15 +41,29 @@ def dijkstra(start, end, graph):
                 dist.update(up)
                 prev[n] = c_node
                 expand = sorted(dist, key=dist.get)
-                for e in expand:
-                    if (e in visited or e not in list(graph[c_node])):
-                        expand.remove(e)
+        i = 0
+        while i < len(expand):
+            if (expand[i] in visited or dist[expand[i]]==999999):
+                expand.pop(i)
+            else :
+                i += 1
         visited += c_node
-    print(prev)
-    print(dist)
+        it_count += 1
+    finish = time.time()
+
+    if (end not in visited):
+        return ([], visited, finish-begin, None, it_count)
+    else:
+        path = [end]
+        while start not in path:
+            path.append(prev[path[len(path)-1]])
+        path.reverse()
+        
+        distance = dist[end]
+        return (path, visited, finish-begin, distance, it_count)
 
 if __name__=="__main__":
     g = nx.DiGraph()
-    graph.read_file('tc1.txt', g)
-    graph.save_img(g)
-    dijkstra('1','6',g)
+    graph.read_file('tc2.txt', g)
+    graph.save_init_img(g)
+    graph.save_fin_img(g, dijkstra('1','5',g)[0])
